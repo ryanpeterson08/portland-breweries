b6e5aa258822cc4f55003c1b71d5dd940fcf2aa9
ryanpeterson08 <Guest@epicodus-10.local>
2016-11-07 09:46:53 -0800

    add basemap to index.html

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+// var createMap = function(){
+//   var breweryMap = L.map('map').setView([45.523171, -122.667256], 10);
+//   return breweryMap;
+// };
+
+$(document).ready(function(){
+  //debugger;
+
+  var mymap = L.map('mapid').setView([45.523171, -122.667256], 13);
+
+  L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+}).addTo(mymap);
+
+
+
+});
