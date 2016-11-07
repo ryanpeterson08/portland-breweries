6387901228648a0a4b30bc94c7d6b0397f3306ed
ryanpeterson08 <Guest@epicodus-10.local>
2016-11-07 10:10:47 -0800

    add function to add to geojson

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,8 @@ var createMap = function(){
 
 $(document).ready(function(){
 
+
+
   L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 	subdomains: 'abcd',
@@ -12,6 +14,10 @@ $(document).ready(function(){
 	maxZoom: 20,
 	ext: 'png'
 }).addTo(createMap());
+
+$.getJSON("data/breweries_final.geojson", function(pub){
+  var brewPubs = L.geoJson(pub).addTo(createMap());
+})
 
 
 
