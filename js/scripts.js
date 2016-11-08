6c2fd48c61c091f6f311249be31b6df8a696bdcc
ryanpeterson08 <Guest@epicodus-10.local>
2016-11-08 15:12:53 -0800

    add locate control

--- js/scripts.js
+++ js/scripts.js
@@ -49,6 +49,7 @@ $(document).ready(function(){
 
   var mapInstance = createMap();
   mapInstance.addControl(searchCtrl);
+  L.control.locate().addTo(mapInstance);
 
   L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
