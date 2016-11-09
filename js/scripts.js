566509b4e1469b7f008eda276976c1e6053b311a
ryanpeterson08 <Guest@epicodus-10.local>
2016-11-09 10:08:24 -0800

    add code to dymanicly generate checklist

--- js/scripts.js
+++ js/scripts.js
@@ -45,55 +45,71 @@ var makeCluster = function(data, map){
     showCoverageOnHover: false
   });
   markers.addLayer(data);
-  map.addLayer(markers);
+  //map.addLayer(markers);
+  return markers;
 }
 
 //Frontend
 $(document).ready(function(){
-
   var pub;
+  var i = 0
+  var checkedArray = [];
   var mapInstance = createMap();
   mapInstance.addControl(searchCtrl);
 
   L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 	ext: 'png'
-}).addTo(mapInstance);
+  }).addTo(mapInstance);
 
-checkedArray = [];
   $.getJSON("data/breweries_final.geojson", function(pub){
     var pubs = addPubsToMap(pub);
     searchCtrl.indexFeatures(pub, ['Brewery', 'Address']);
-    mapInstance.addLayer(pubs);
-    //makeCluster(pubs, mapInstance);
+    //mapInstance.addLayer(pubs);
+    var pubCluster = makeCluster(pubs, mapInstance);
+    mapInstance.addLayer(pubCluster);
+
+    pub.features.forEach(function(each) {
+      if (i < 30) {
+        $("#column-1").append("<input type='checkbox' name='visited-pubs' value='" + i + "'>" + each.properties.Brewery + "<br>")
+        i++;
+      } else if (i < 60) {
+        $("#column-2").append("<input type='checkbox' name='visited-pubs' value='" + i + "'>" + each.properties.Brewery + "<br>")
+        i++;
+      } else if (i < 90) {
+        $("#column-3").append("<input type='checkbox' name='visited-pubs' value='" + i + "'>" + each.properties.Brewery + "<br>")
+        i++;
+      } else {
+        $("#column-4").append("<input type='checkbox' name='visited-pubs' value='" + i + "'>" + each.properties.Brewery + "<br>")
+        i++;
+      }
+    });
 
 
-
-    $(".filter-button").submit(function(event){
+    $("#filter-button").submit(function(event){
       event.preventDefault();
       $("input:checkbox[name=visited-pubs]:checked").each(function(){
          checkedArray.push($(this).val())
        })
 
-
-      // pub.features.forEach(function(each){
-      //   if (each.properties.)
-      //    each.properties.Visited = true;
-      //    console.log(each);
-      //  });
-
       checkedArray.forEach(function(each){
         pub.features[each].properties.Visited = true;
       })
 
-      console.log(pub);
-      mapInstance.removeLayer(pubs);
+      //remove all pub icons then replace with with pubs that have Visited: false
+      mapInstance.removeLayer(pubCluster);
       pubs = addPubsToMap(pub);
-      mapInstance.addLayer(pubs)
+      var newPubCluster =  makeCluster(pubs, mapInstance);
+      mapInstance.addLayer(newPubCluster);
+
 
     });
       $("#new-points").click(function(){
-      console.log(pub);
+      pub.features.forEach(function(each){
+         each.properties.Visited = false;
+       });
+      pubs = addPubsToMap(pub);
+      mapInstance.addLayer(pubs)
 
     })
   });
